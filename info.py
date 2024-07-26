6c40967d69dad73726ec122622f48672e2cd5480
Abhibotssz <155344413+Abhibotssz@users.noreply.github.com>
2024-07-26 06:20:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://super-mehetabel-tjbot-469d39c2.koyeb.app/")
+URL = environ.get("URL", "https://cognitive-betteann-namanrathoreji-3d58d83f.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
